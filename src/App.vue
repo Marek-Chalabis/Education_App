04c755310a039a5727b4cd1e15b21f293ee1d5f5
zulaber <chalabismarek@gmail.com>
2020-09-28 21:36:51 +0200

    Added form for questions

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div>
       <!-- TODO style it after adding navbar -->
       <themeSwitcher />
+      <!-- TODO change place after addind users -->
+      <createNotesPopup />
       <b-container fluid>
         <b-row>
           <b-col></b-col>
@@ -19,12 +21,14 @@
 <script>
 import questionView from "./components/questionView";
 import themeSwitcher from "./components/themeSwitcher";
+import createNotesPopup from "./components/CreateNotes/createNotesPopup";
 
 export default {
   name: "App",
   components: {
     questionView,
     themeSwitcher,
+    createNotesPopup,
   },
 };
 </script>
